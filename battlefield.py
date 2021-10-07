1484432b8ec8648293f095ac0b604167a1395b3d
Samuel Guthrie <lottguthrie@yahoo.com>
2021-10-07 16:19:25 -0500

    Completed battle method. linked oponent identification to the battle method. entered dialogue and depleting health requirements. Added if else statements to provide options as well as while statements to verify the damage to oponent.

--- battlefield.py
+++ battlefield.py
@@ -6,9 +6,13 @@ from robot import Robot
 
 
 class Battlefield:
-    def __init__(self):
+    def __init__(self, fleet, herd, dinosaur, robot, damage, die):
         self.fleet = Fleet()
         self.herd = Herd()
+        self.dinosaur = Dinosaur[0]
+        self.robot = Robot[0]
+        self.damage  = damage[0]
+        self.die = die()
 
     def run_game(self):
         pass
@@ -18,7 +22,37 @@ class Battlefield:
         
     
     def battle(self):
-        self.dino
+        self.battle_begins = input("Are you ready for the ultimate game of survival? If so enter yes to begin the war for survival of the fittest!")
+        if self.battle_begins == "yes":
+            print("Let the battle begin")
+            print(f"{self.dinosaur[0]} is your fighter")
+            print(f"Your Teams health is {self.dinosaur.health[0]}") 
+            while self.battle_begins == "yes":
+                self.run_game()
+            while(self.show_dino_oponent_options()and self.show_robo_oponent_options()):
+                self.dinosaur.attack(self.robot)
+                self.robot.health - 10
+                self.robot.attack(self.dinosaur)
+                self.dinosaur.health - 10
+                if self.robot.health == 50:
+                    print("Your oponent {robot[0]} is fading fast")
+                elif self.robot.health == 50:
+                    print("Your oponent {robot[0]} is fading fast")
+                elif self.dinosaur.health == 0:
+                    print("{dinosaur[0]} has died")
+                elif self.robot.health == 0:
+                    print("Your oponent {robot[0]} has died") 
+                else:
+                    print("The battle rages on!")    
+        else:
+            print("You are not ready!Please start over")
+            def start_over(self):
+                self.display_welcom()
+       
+
+
+
+
 
     def dino_turn(self):
         self.dinosaur = input(f"Enter 1 for {Dinosaur.name[0]}, 2 for {Dinosaur.name[1]}, 3 for {Dinosaur.name[2]}, 4 for {Dinosaur.name[3]}")
